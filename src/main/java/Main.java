e4a3b398c6a3ef55e3d1fd33045e19670b9160d0
Avila Salvador A <salvador.avila@thermofisher.com>
2017-12-07 07:15:49 -0600

    Resources relative path & LOGGER

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,41 +4,46 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class Main {
-
 	// CSV headers
 	final static String[] record_headers = new String[]{
 		"E","B","C","D","F","G","E","E","E","E","E","E","E","E","E","E","E"};
 
 	// Test endpoints
-	final static String endpoint1 = "https://requestb.in/1fiesf61";
-	//final static String endpoint2 = "http://127.0.0.1:8000";
+	//final static String endpoint1 = "https://requestb.in/1fiesf61";
+	final static String endpoint3 = "https://n20bcm7mi0.execute-api.us-east-1.amazonaws.com/dev/molspec-hackathon-publish-device-data-record";
 
-	// TODO: Get relative path to /resources/*.csv
-	final static String csv_filepath = "/Users/salvador.avila/javacode/device_emulator/src/main/resources/data.csv";
+	// Logging
+	private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
 
 	public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
-		// Stream file
-		List<HashMap> records = processFile(csv_filepath);
+		LOGGER.setLevel(Level.INFO);
+
+		// Load csv file
+		ClassLoader classloader = Main.class.getClassLoader();
+		InputStream is = classloader.getResourceAsStream(("data.csv"));
+
+		// Map csv lines into dictionaries
+		List<HashMap> records = processFile(is);
 		for(HashMap record : records) {
 			// Make request
-			doPOST(endpoint1, record);
+			doPOST(endpoint3, record);
 
 			// Wait
 			TimeUnit.SECONDS.sleep(5);
 		}
 	}
 
-	public static List<HashMap> processFile(String file_path) {
+	public static List<HashMap> processFile(InputStream is) {
 		List<HashMap> record_list = new ArrayList<>();
 		try {
-			File file = new File(file_path);
-			InputStream file_stream = new FileInputStream(file);
-			BufferedReader br = new BufferedReader(new InputStreamReader(file_stream));
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-			// Function : Map csv line to dictionary
+			// Function : Map line to dictionary
 			record_list = br.lines().skip(1).map(mapToRecordDictionary).collect(Collectors.toList());
 			br.close();
 		} catch (FileNotFoundException e) {
@@ -56,13 +61,14 @@ public class Main {
 		for (int i=0; i < values.length; i++){
 			record.put(record_headers[i], values[i]);
 		}
+
 		return record;
 	};
 
 	public static void doPOST(String request_url, HashMap record) throws UnsupportedEncodingException {
-		System.err.println("\nRecord: " + record.toString());
-		String json = new Gson().toJson(record);
-		System.out.println("JSON: " + json);
+		String message = new Gson().toJson(record);
+		System.out.println();
+		LOGGER.info("JSON: " + message);
 
 		try{
 			URL url = new URL(request_url);
@@ -71,19 +77,19 @@ public class Main {
 			// Prepare request
 			con.setRequestMethod("POST");
 			con.setRequestProperty("Content-Type", "\"Content-Type\", \"application/x-www-form-urlencoded");
-			con.setRequestProperty("Content-Length", String.valueOf(json));
+			con.setRequestProperty("Content-Length", String.valueOf(message));
 			con.setDoOutput(true);
 
 			// Write
 			OutputStreamWriter os = new OutputStreamWriter(con.getOutputStream());
-			os.write(json);
+			os.write(message);
 			os.flush();
 			os.close();
 
 			// Response
 			int responseCode = con.getResponseCode();
-			System.out.println("Sending 'POST' request to URL : " + url);
-			System.out.println("Response Code : " + responseCode);
+			LOGGER.info("Sending 'POST' request to URL : " + url);
+			LOGGER.info("Response Code : " + responseCode);
 
 		}catch(MalformedURLException e){
 			e.printStackTrace();
